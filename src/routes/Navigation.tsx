35760f1e76010331372e0bc61f3e4815ca6a9856
Alexis Manuel Hurtado García <alexismhgdotanime@gmail.com>
2023-05-13 06:19:03 +0200

    Lazyload basico implementado

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,49 +1,38 @@
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import { BrowserRouter, Routes, Route, NavLink, Navigate } from "react-router-dom";
 import reactLogo from "../assets/react.svg";
-import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages"
+import { routes } from '../01-lazyload/router/routes';
+import { Suspense } from "react";
 
 export const Navigation = () => {
     return (
-        <BrowserRouter>
-            <div className="text-white text-center text-3xl w-52 bg-gray-800 mr-5">
-                <nav>
-                    <img className="h-28 mt-4 mx-auto" src={reactLogo} alt="React Logo" />
-                    <ul className="my-5">
-                        <li>
-                            <NavLink
-                                to="/lazy1"
-                                className={({ isActive }) => (isActive ? "text-gray-500" : "")}
-                            >
-                                LazyPage1
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink
-                                to="/lazy2"
-                                className={({ isActive }) => (isActive ? "text-gray-500" : "")}
-                            >
-                                LazyPage2
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink
-                                to="/lazy3"
-                                className={({ isActive }) => (isActive ? "text-gray-500" : "")}
-                            >
-                                LazyPage3
-                            </NavLink>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-            <Routes>
-                <Route path="/lazy1" element={<LazyPage1 />} />
-                <Route path="/lazy2" element={<LazyPage2 />} />
-                <Route path="/lazy3" element={<LazyPage3 />} />
+        <Suspense fallback={<h1 className="text-center">Loading</h1>}>
+            <BrowserRouter>
+                <div className="text-white text-center text-3xl w-52 bg-gray-800 mr-5">
+                    <nav>
+                        <img className="h-28 mt-4 mx-auto" src={reactLogo} alt="React Logo" />
+                        <ul className="my-5">
+                            {routes.map(({ path, name }) =>
+                            (
+                                <li key={name}>
+                                    <NavLink
+                                        to={path}
+                                        className={({ isActive }) => (isActive ? "text-gray-500" : "")}
+                                    >
+                                        {name}
+                                    </NavLink>
+                                </li>
+                            )
+                            )}
 
-                <Route path="/*" element={<LazyPage1 />} />
-            </Routes>
-        </BrowserRouter>
+                        </ul>
+                    </nav>
+                </div>
+                <Routes>
+                    {routes.map(({ name, path, Component }) => (<Route key={name} path={path} Component={Component} />))}
+                    <Route path='*' element={<Navigate to={routes[0].path} replace={true} />} />
+                </Routes>
+            </BrowserRouter>
+        </Suspense>
 
     );
 };
