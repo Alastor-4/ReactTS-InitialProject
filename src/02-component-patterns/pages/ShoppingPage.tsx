06741489ebc7e49626780845f5030c8603c350db
Alexis Manuel Hurtado García <alexismhgdotanime@gmail.com>
2023-05-19 18:08:36 +0200

    forms iniciado

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++
@@ -1,5 +0,0 @@
-export const ShoppingPage = () => {
-    return (
-        <div><h1>ShoppingPage</h1></div>
-    )
-}
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import reactLogo from "../assets/react.svg";
-import { ShoppingPage } from "../02-component-patterns/pages/ShoppingPage";
 
 export const Navigation = () => {
     return (
@@ -14,7 +13,7 @@ export const Navigation = () => {
                                 to="/home"
                                 className={({ isActive }) => (isActive ? "text-gray-500" : "")}
                             >
-                                Shopping
+                                Home
                             </NavLink>
                         </li>
                         <li>
@@ -39,7 +38,7 @@ export const Navigation = () => {
             <Routes>
                 <Route path="about" element={<h1>About Page</h1>} />
                 <Route path="users" element={<h1>Users Page</h1>} />
-                <Route path="home" element={<ShoppingPage />} />
+                <Route path="home" element={<h1>Home Page</h1>} />
 
                 <Route path="/*" element={<h1>Home Page</h1>} />
             </Routes>
